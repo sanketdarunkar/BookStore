dedee34c9e8881c16791c9e692cced6379f94186
sanketdarunkar <sanketdarunkarsd123@gmail.com>
2024-05-17 23:07:17 +0530

    first commit

---
+++ Frontend/src/components/aboutUs/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar";
+import About from "../About";
+import Footer from "../Footer";
+function AboutUs() {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Footer />
+    </>
+  );
+}
+
+export default AboutUs;
---
+++ Frontend/src/components/contact/Contactus.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Contact from "../Contact";
+import Footer from "../Footer";
+
+function Contactus() {
+  return (
+    <>
+      <Navbar />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default Contactus;
